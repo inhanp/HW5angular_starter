12c4c524e0a5bdf1dcf8274d85484b93a8a0a878
Inhan Park <inhan.park.95@gmail.com>
2021-08-11 12:51:38 +0900

    rank finished

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -3,17 +3,35 @@ import { first } from 'rxjs/operators';
 
 import { User } from '../_models/user';
 import { UserService } from '../_services/user.service';
-import {RankComponent} from '../rank/rank.component';
+import {PArecordService} from '../_services/parecord.service';
 
 @Component({ templateUrl: 'admin.component.html' })
 export class AdminComponent implements OnInit {
   users: User[] = [];
+  records = [];
+  avgcalories: number;
+  avgminutes: number;
+  minuterate: number;
+  rank: number;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService,
+              private recordService: PArecordService) { }
 
   ngOnInit() {
     this.userService.getAll().pipe(first()).subscribe(users => {
-      this.users = users;
+      users.forEach(user => {
+        this.recordService.getAverage(user.username).pipe(first()).subscribe(record => {
+          console.log(record);
+          this.records.push(record);
+          // sort method
+          this.records.sort((a, b) => {
+            return b.avgminutes - a.avgminutes;
+           });
+          this.records.forEach( (value) => {
+            value.rank = this.records.indexOf(value) + 1;
+          });
+        });
+      });
     });
   }
 }
